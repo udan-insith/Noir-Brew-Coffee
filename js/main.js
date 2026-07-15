80f6097512a45ecbe75b00dcafcdf13a06239f38
udan-insith <udaninsith23613@gmail.com>
2026-07-15 19:41:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 // MAIN js - This is some extra features
 (function NB_Main() {
   "use strict";
+
+  window.NB = window.NB || {};
+  const toast = (...args) =>
+    window.addToast ? window.addToast(...args) : console.log(...args);
 });
